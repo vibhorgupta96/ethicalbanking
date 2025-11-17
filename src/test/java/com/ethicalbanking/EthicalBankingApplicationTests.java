539395ce1880cfe1d231002e59b76d39575e4bbf
Vibhor Gupta <19585120+vibhorgupta96@users.noreply.github.com>
2025-11-17 22:17:52 +0530

    Initial commit

---
+++ src/test/java/com/ethicalbanking/EthicalBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ethicalbanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EthicalBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
